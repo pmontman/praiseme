d21f56a6644f18fab30e41bd8b9a8cb159ec7b68
pmont <pmontm@gmail.com>
2018-02-22 10:16:26 +1100

    faerst kommeet!

---
+++ R/praise.R
@@ -0,0 +1,9 @@
+#'@title Praise the User
+#'@description This function delivers praise to the user
+#'
+#' @examples
+#' praise()
+#'@export
+praise <- function() {
+  paste0("You are awesome!!!")
+}
